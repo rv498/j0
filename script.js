5be3d80ab30dcfd0470a49e5a180eb66cf4fe9c5
Sang Han <hanbellevue1@gmail.com>
2020-03-31 14:18:30 -0700

    second commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+(function () {
+
+    $('#btnShowName').click(function () {
+        console.log("Emily Han");
+    });
+
+    $('#btnShowAge').click(function () {
+        console.log("14");
+    });
+
+    $('#btnShowFavHobby').click(function () {
+        console.log("Drawing");
+    });
+
+    $('#btnShowFavAnimal').click(function () {
+        console.log("Elephant");
+    });
+})();
